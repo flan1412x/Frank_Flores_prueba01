0a1187a317eb56a77a381b5ea30fbe92b635a732
Frank23 <1214kurobakid@gmail.com>
2023-10-24 18:45:28 -0500

    FrankFlores 6:45pm

--- app/src/main/java/com/example/frank_flores_prueba01/MainActivity3.java
+++ app/src/main/java/com/example/frank_flores_prueba01/MainActivity3.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -22,4 +23,6 @@ public class MainActivity3 extends AppCompatActivity {
             }
         });
     }
+
+
 }
