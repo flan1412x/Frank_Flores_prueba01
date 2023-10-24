54262be834f428367e927a39fffc7ceb050fd76f
Frank23 <1214kurobakid@gmail.com>
2023-10-24 18:32:21 -0500

    FrankFlores 6:30pm

--- app/src/main/java/com/example/frank_flores_prueba01/MainActivity.java
+++ app/src/main/java/com/example/frank_flores_prueba01/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,8 +47,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public  static  void NumInvertido(){
 
 
-    }
+
+
+
 }
